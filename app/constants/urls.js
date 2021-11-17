6109f35b9b05991e562f1b6f4c26835ec6ed1e86
ruafvi4 <Irina.N.FROLOVA@raiffeisen.ru>
2021-11-17 19:29:15 +0000

    fix url

--- app/constants/urls.js
+++ app/constants/urls.js
@@ -1,3 +1,3 @@
-export const GET_DATA = (clientId) => `https://team-16-back.rba.codenrock.com/api/v1/client/${clientId}/token`;
+export const GET_DATA = (clientId) => `https://team-16-back.rba.codenrock.com/api/v1/client/${clientId}`;
 export const SUBSCRIPTION = (id) => `https://pay-test.raif.ru/api/nspc-mock/qrs/${id}/subscribe`;
 export const QR_INFO = (qrId) => `https://pay-test.raif.ru/api/nspc-mock/qrs/${qrId}`
